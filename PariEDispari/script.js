600eeedfe0a27145b28c428e6cd22042de494d3b
rubacodes <rubacodes@gmail.com>
2022-05-06 15:23:30 +0200

    somma dei due numeri 1-5

--- PariEDispari/script.js
+++ PariEDispari/script.js
@@ -1,5 +1,5 @@
 // ---FUNZIONI---
-//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). *****implementazione generale con min e max
 function randomNumberRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -20,3 +20,7 @@ let number;
 do {
     number = Number(prompt('Inserire un numero da 1 a 5'))
 } while (number > 5);
+
+//Sommiamo i due numeri
+const sum = number + randomNumberRange(1,5);
+console.log(sum);
