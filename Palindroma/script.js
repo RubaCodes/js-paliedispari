95303ddca19b0a3723778112b1d18746f7813d46
rubacodes <rubacodes@gmail.com>
2022-05-06 15:05:05 +0200

    realizzata funzione di analisi parola in O(n/2)

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,8 +1,21 @@
 // ---FUNZIONI---
 
 //funzione che ritorna true se la parola e' palindroma
-function isPalindroma(word){
-    return word.split('');
+function isPalindroma(word) {
+    const array = word.split('');
+    let indexUp = 0;
+    let indexDown = array.length - 1;
+    console.log(indexDown, indexUp);
+    
+    while (indexUp <= indexDown) {
+        if (array[indexUp] === array[indexDown]) {
+            indexUp++;
+            indexDown--;
+        }else{
+            return false;
+        } 
+    }
+    return true;
 }
 
 
@@ -10,7 +23,7 @@ function isPalindroma(word){
 
 //parrola in input dall'utente
 const userInput = prompt('Inserisci una parola')
-//chiamo la funzione 
+//chiamo la funzione
 const result = isPalindroma(userInput);
 //stampo il risultato
 console.log(result);
