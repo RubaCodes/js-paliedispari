95319be21f81ea58bb8374ce4ec1e927a8a0361a
rubacodes <rubacodes@gmail.com>
2022-05-06 15:17:59 +0200

    inserimento numero utente con validazione

---
+++ PariEDispari/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// ---MAIN PROGRAM---
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+let number;
+do {
+    number = Number(prompt('Inserire un numero da 1 a 5'))
+} while (number > 5);
