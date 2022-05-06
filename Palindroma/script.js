17041e423649851f1c2f6c92aa9e66c9ec65de5a
rubacodes <rubacodes@gmail.com>
2022-05-06 14:50:21 +0200

    aggiunti commenti

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,11 +1,16 @@
 // ---FUNZIONI---
+
+//funzione che ritorna true se la parola e' palindroma
 function isPalindroma(word){
     return word.split('');
 }
 
 
 // --- PROGRAMMA---
-const userInput = prompt('Inserisci una parola')
 
+//parrola in input dall'utente
+const userInput = prompt('Inserisci una parola')
+//chiamo la funzione 
 const result = isPalindroma(userInput);
+//stampo il risultato
 console.log(result);
